23de508b3cd9c6569786871287db68e21138ad16
StormLight14 <stormpower44@protonmail.com>
2024-03-29 21:01:34 -0700

    give fireballs a limited lifetime

--- hazards/fireball/fireball.gd
+++ hazards/fireball/fireball.gd
@@ -1,12 +1,21 @@
 extends Area2D
 
+@onready var lifetime_timer = $LifetimeTimer
+
 var direction = Vector2(0, 0)
 var speed = 100.0
+var lifetime = 5 # seconds
 var hazard_type = "fireball"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _ready():
+	lifetime_timer.wait_time = lifetime
+	lifetime_timer.start()
+
 func _process(delta):
 	global_position += direction * speed * delta
 
 func _on_area_entered(area):
+	queue_free()
+
+func _on_lifetime_timeout():
 	queue_free()
