56d7404c0f9821a90ebfe6e9119aaa33c2114490
StormLight14 <stormpower44@protonmail.com>
2024-04-04 19:23:53 -0700

    add climbing stamina

--- player/player.gd
+++ player/player.gd
@@ -10,20 +10,25 @@ const MAX_SPEED = 150.0
 const ACCELERATION = 850.0
 const JUMP_VELOCITY = -275.0
 const CLIMB_SPEED = 100.0
-const STAMINA = 1500 # 1ms of holding takes 1 stamina; 1 ms of climbing takes 2
+const MAX_STAMINA = 1500
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var state = IDLE
+var player_stamina = MAX_STAMINA
+var can_regenerate_stamina = true
 
 @onready var climb_checker_left = %ClimbCheckerLeft
 @onready var climb_checker_right = %ClimbCheckerRight
+@onready var progress_bar = %ProgressBar
+@onready var hearts = %Hearts
+@onready var stamina_timer = %StaminaTimer
 
 @export var player_hearts = 3
-@onready var hearts = %Hearts
 
 signal killed
 
 func _ready():
+	update_climb_ui()
 	update_hearts()
 
 func _physics_process(delta):
@@ -36,29 +41,44 @@ func _physics_process(delta):
 			state_climbing(delta)
 
 	handle_climbing(delta)
-	
-
 	move_and_slide()
-	
+
 func state_idle(delta):
 	handle_movement(delta)
 	handle_jump()
 	handle_gravity(delta, 1)
+	handle_stamina(delta)
 	
 func state_running(delta):
 	handle_movement(delta)
 	handle_jump()
 	handle_gravity(delta, 1)
+	handle_stamina(delta)
 	
 func state_climbing(delta):
 	handle_climbing(delta)
+	
+func handle_stamina(delta):
+	print(can_regenerate_stamina)
+	if player_stamina < 0:
+		player_stamina = 0
+		can_regenerate_stamina = false
+	
+	if can_regenerate_stamina:
+		if player_stamina < MAX_STAMINA:
+			player_stamina += 150 * delta
+		else:
+			player_stamina = MAX_STAMINA
+	else:
+		stamina_timer.start()
+	update_climb_ui()
 	
 func handle_climbing(delta):
 	var left_has_wall = climb_checker_left.is_colliding()
 	var right_has_wall = climb_checker_right.is_colliding()
 	var on_wall = left_has_wall or right_has_wall
 	
-	if Input.is_action_pressed("climb") and on_wall and not is_on_floor():
+	if Input.is_action_pressed("climb") and on_wall and not is_on_floor() and player_stamina > 0:
 		if state != CLIMBING:
 			state = CLIMBING
 			
@@ -68,14 +88,23 @@ func handle_climbing(delta):
 				velocity.x = -JUMP_VELOCITY / 1.5
 			elif right_has_wall:
 				velocity.x = JUMP_VELOCITY / 1.5
+			player_stamina -= 200 * delta
 		elif left_has_wall and Input.is_action_pressed("left") or right_has_wall and Input.is_action_pressed("right"):
 			velocity.y = -CLIMB_SPEED
+			player_stamina -= 200 * delta
 		elif left_has_wall and Input.is_action_pressed("right") or right_has_wall and Input.is_action_pressed("left"):
 			velocity.y = CLIMB_SPEED
+			player_stamina -= 200 * delta
 		else:
+			player_stamina -= 100 * delta
 			velocity.y = move_toward(velocity.y, 0, ACCELERATION * delta)
+			
+		update_climb_ui()
 	else:
 		state = IDLE
+		
+func update_climb_ui():
+	progress_bar.value = player_stamina
 	
 func handle_movement(delta):
 	var direction = Input.get_axis("left", "right")
@@ -111,3 +140,6 @@ func _on_hurtbox_area_entered(area):
 	killed.emit()
 	player_hearts = 3
 	update_hearts()
+
+func _on_stamina_timer_timeout():
+	can_regenerate_stamina = true
