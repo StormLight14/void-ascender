f82a887e0ec7a218a10613ea93d56e71c4f2a3a6
StormLight14 <stormpower44@protonmail.com>
2024-04-20 13:55:54 -0700

    add idle animation

--- player/player.gd
+++ player/player.gd
@@ -22,10 +22,12 @@ var can_regenerate_stamina = true
 @onready var progress_bar = %ProgressBar
 @onready var hearts = %Hearts
 @onready var stamina_timer = %StaminaTimer
+@onready var animated_sprite_2d = %AnimatedSprite2D
 
 signal killed
 
 func _ready():
+	animated_sprite_2d.play("idle")
 	update_climb_ui()
 	#update_hearts()
 
@@ -42,18 +44,26 @@ func _physics_process(delta):
 	move_and_slide()
 
 func state_idle(delta):
+	if animated_sprite_2d.animation != "idle":
+		animated_sprite_2d.play("idle")
+	if velocity != Vector2.ZERO:
+		state = RUNNING
 	handle_movement(delta)
 	handle_jump()
 	handle_gravity(delta, 1)
 	handle_stamina(delta)
 	
 func state_running(delta):
+	if animated_sprite_2d.animation != "running":
+		animated_sprite_2d.play("running")
 	handle_movement(delta)
 	handle_jump()
 	handle_gravity(delta, 1)
 	handle_stamina(delta)
 	
 func state_climbing(delta):
+	if animated_sprite_2d.animation != "climbing":
+		animated_sprite_2d.play("climbing")
 	handle_climbing(delta)
 	
 func handle_stamina(delta):
@@ -74,12 +84,12 @@ func handle_climbing(delta):
 	var right_has_wall = climb_checker_right.is_colliding()
 	var on_wall = left_has_wall or right_has_wall
 	
-	if Input.is_action_pressed("climb") and on_wall and not is_on_floor() and player_stamina > 0:
+	if Input.is_action_pressed("climb") and on_wall and not is_on_floor() and player_stamina > 0 and stamina_timer.is_stopped():
 		if left_has_wall:
-			%Sprite2D.rotation_degrees = 90
+			animated_sprite_2d.rotation_degrees = 90
 			%CollisionShape2D.rotation_degrees = 180
 		if right_has_wall:
-			%Sprite2D.rotation_degrees = -90
+			animated_sprite_2d.rotation_degrees = -90
 			%CollisionShape2D.rotation_degrees = 0
 		if state != CLIMBING:
 			state = CLIMBING
@@ -104,7 +114,7 @@ func handle_climbing(delta):
 			
 		update_climb_ui()
 	else:
-		%Sprite2D.rotation_degrees = 0
+		animated_sprite_2d.rotation_degrees = 0
 		%CollisionShape2D.rotation_degrees = 90
 		state = IDLE
 		
@@ -140,6 +150,3 @@ func update_hearts():
 
 func _on_hurtbox_area_entered(area):
 	killed.emit()
-
-func _on_stamina_timer_timeout():
-	can_regenerate_stamina = true
