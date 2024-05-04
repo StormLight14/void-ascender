b03dd1066b18be7aa3dd7e33207e5f154ad46080
StormLight14 <stormpower44@protonmail.com>
2024-05-03 20:48:02 -0700

    add dialog event trigger

--- level/event_trigger.gd
+++ level/event_trigger.gd
@@ -4,11 +4,14 @@ extends Area2D
 @export var event = ""
 @export var str_args: Array[String]
 @export var num_args: Array[float]
-@export var vec2_args: Array[Vector2] = [Vector2(0, -15)]
+@export var vec2_args: Array[Vector2]
 
 func _on_body_entered(body):
-	if trigger_limit > 0:
-		match event:
-			"broadcast":
-				Global.broadcast(str_args, num_args[0], num_args[1], vec2_args[0]) # messages, font size, seconds, offset
+	match event:
+		"broadcast":
+			Global.broadcast(str_args, num_args[0], num_args[1], vec2_args[0]) # messages, font size, seconds, offset
+		"dialog":
+			Dialogic.start(str_args[0])
 	trigger_limit -= 1
+	if trigger_limit <= 0:
+		queue_free()
