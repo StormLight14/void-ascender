fbb47914d40d938873311f19a04d798ea412f871
StormLight14 <stormpower44@protonmail.com>
2024-05-05 17:45:55 -0700

    make fireball spawner direction automatic

--- hazards/fireball/fireball_spawner.gd
+++ hazards/fireball/fireball_spawner.gd
@@ -3,22 +3,23 @@ extends Node2D
 @onready var spawn_timer = $SpawnTimer
 @onready var sprite_2d = $Sprite2D
 
-@export var direction = Vector2(1, 0)
 @export var spawn_delay = 3
 @export var fireball_speed = 150
 @export var active = true
 
 signal spawn_fireball(fireball_direction, fireball_speed, spawn_position)
 
+var direction = Vector2.ZERO
+
 func _ready():
+	direction = Vector2.RIGHT.rotated(rotation)
+	print(direction)
 	if active:
 		spawn_timer.wait_time = spawn_delay
 		spawn_timer.start()
 	else:
 		visible = false
 		
-	if direction != Vector2.ZERO:
-		sprite_2d.look_at(direction * 1000)
 
 func _on_spawn_timer_timeout():
 	spawn_fireball.emit(direction, fireball_speed, global_position)
