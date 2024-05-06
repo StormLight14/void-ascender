a745d2ddbeaa8cfeb96923975d90890be2833e4c
StormLight14 <stormpower44@protonmail.com>
2024-05-05 18:03:28 -0700

    add dynamic fire trail direction

--- hazards/fireball/fireball.gd
+++ hazards/fireball/fireball.gd
@@ -1,12 +1,14 @@
 extends Area2D
 
 @onready var lifetime_timer = $LifetimeTimer
+@onready var cpu_particles_2d = $CPUParticles2D
 
 var direction = Vector2(0, 0)
 var speed = 100.0
 var lifetime = 5 # seconds
 
 func _ready():
+	cpu_particles_2d.direction = direction
 	lifetime_timer.wait_time = lifetime
 	lifetime_timer.start()
 
