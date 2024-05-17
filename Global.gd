547e2d510ea3e23febaa6bd9400987c0662708a4
StormLight14 <stormpower44@protonmail.com>
2024-05-17 14:16:22 -0700

    add sign "typing" effect

--- Global.gd
+++ Global.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var ui_open = false
+var sign_char_index = 0
+var sign_text = "placeholder"
 
 #func _process(_delta):
 #	if Dialogic.current_timeline != null:
@@ -16,10 +18,21 @@ func broadcast(messages, seconds = 5, font_size = 8, offset = Vector2(0, -30)):
 	
 func show_sign_text(text):
 	%Sign.visible = true
-	%SignLabel.text = text
+	sign_char_index = 0
+	
+	if text:
+		sign_text = text
+
+	%CharDisplayTimer.start()
+	
+func _on_char_display_timer_timeout():
+	if sign_char_index < len(sign_text):
+		%SignLabel.text += sign_text[sign_char_index]
+		sign_char_index += 1
 	
 func _on_broadcast_timer_timeout():
 	%AnimationPlayer.play("broadcast_fade_out")
 
 func save_game():
 	pass
+
