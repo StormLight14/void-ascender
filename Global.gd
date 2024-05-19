f202be4e73719083dbd194d19030e970768731ea
StormLight14 <stormpower44@protonmail.com>
2024-05-18 20:26:58 -0700

    fix up signs a bit

--- Global.gd
+++ Global.gd
@@ -3,10 +3,13 @@ extends Node2D
 var ui_open = false
 var sign_char_index = 0
 var sign_text = "placeholder"
+var sign_finished = false
 
-#func _process(_delta):
-#	if Dialogic.current_timeline != null:
-#		get_tree().paused = true
+func _process(_delta):
+	if Input.is_action_just_pressed("ui_accept") and sign_finished:
+		get_tree().paused = false
+		%Sign.visible = false
+
 
 func broadcast(messages, seconds = 5, font_size = 8, offset = Vector2(0, -30)):
 	%BroadcastLabel.text = messages[0]
@@ -18,6 +21,9 @@ func broadcast(messages, seconds = 5, font_size = 8, offset = Vector2(0, -30)):
 	
 func show_sign_text(text):
 	%Sign.visible = true
+	%SignLabel.text = ""
+	sign_finished = false
+	get_tree().paused = true
 	sign_char_index = 0
 	
 	if text:
@@ -29,6 +35,8 @@ func _on_char_display_timer_timeout():
 	if sign_char_index < len(sign_text):
 		%SignLabel.text += sign_text[sign_char_index]
 		sign_char_index += 1
+	else:
+		sign_finished = true
 	
 func _on_broadcast_timer_timeout():
 	%AnimationPlayer.play("broadcast_fade_out")
