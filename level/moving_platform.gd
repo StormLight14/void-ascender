d17092268618ba92b6b9b2e3124feb9b5a295510
StormLight14 <stormpower44@protonmail.com>
2024-05-22 19:16:46 -0700

    platforms follow set path

--- level/moving_platform.gd
+++ level/moving_platform.gd
@@ -1,11 +1,29 @@
-extends Path2D
+extends Node2D
 
+# alt + click to move platform in editor
 
-# Called when the node enters the scene tree for the first time.
+@onready var animated_sprite_2d = %AnimatedSprite2D
+@onready var animation_player = $AnimationPlayer
+
+@export var platform_type = "automatic" # possible choices: automatic, triggered
+@export var move_time = 1.0 # in seconds
+@export var reverses = true # platform reverses when it reaches end of path
+@export var backwards_speed_scale = 0.7
+
 func _ready():
-	pass # Replace with function body.
+	if platform_type == "automatic":
+		move()
+	
+func move():
+	animation_player.speed_scale = 1 / move_time
+	animation_player.play("move")
+	
+func move_backward():
+	animation_player.speed_scale = 1 / move_time * backwards_speed_scale
+	animation_player.play("move_backward")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "move" and reverses:
+		move_backward()
+	elif anim_name == "move_backward" and platform_type == "automatic":
+		move()
