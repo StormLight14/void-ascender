4f50c4e4500a4a58a5621923b417a276b56b4c80
StormLight14 <stormpower44@protonmail.com>
2024-05-25 13:00:46 -0700

    handle platform movement in code

--- level/moving_platform.gd
+++ level/moving_platform.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Path2D
 
 # alt + click to move platform in editor
 
@@ -6,24 +6,29 @@ extends Node2D
 @onready var animation_player = $AnimationPlayer
 
 @export var platform_type = "automatic" # possible choices: automatic, triggered
-@export var move_time = 1.0 # in seconds
+@export var move_speed = 300.0
 @export var reverses = true # platform reverses when it reaches end of path
 @export var backwards_speed_scale = 0.7
 
+enum { Stopped, Forward, Backward }
+
+var movement = Stopped
+
 func _ready():
 	if platform_type == "automatic":
-		move()
-	
-func move():
-	animation_player.speed_scale = 1 / move_time
-	animation_player.play("move")
-	
-func move_backward():
-	animation_player.speed_scale = 1 / move_time * backwards_speed_scale
-	animation_player.play("move_backward")
-
-func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "move" and reverses:
-		move_backward()
-	elif anim_name == "move_backward" and platform_type == "automatic":
-		move()
+		movement = Forward
+		
+func _physics_process(delta):
+	if movement == Forward:
+		if %PathFollow2D.progress < curve.get_baked_length() - move_speed * delta:
+			%PathFollow2D.progress += move_speed * delta
+		else:
+			%PathFollow2D.progress_ratio = curve.get_baked_length()
+			if reverses:
+				movement = Backward
+	elif movement == Backward:
+		if %PathFollow2D.progress > 0 + move_speed * delta:
+			%PathFollow2D.progress -= move_speed * delta
+		else:
+			%PathFollow2D.progress_ratio = 0.0
+			movement = Forward
