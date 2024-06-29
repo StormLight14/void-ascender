25496936ebb6ba55e98b90581dcca536d5eb6177
StormLight14 <stormpower44@protonmail.com>
2024-06-29 14:15:40 -0600

    change "continue" to more accurate "levels"

--- autoloads/Global.gd
+++ autoloads/Global.gd
@@ -10,12 +10,28 @@ func broadcast(messages, seconds = 5, font_size = 8, offset = Vector2(0, -30)):
 	%BroadcastTimer.start()
 	%AnimationPlayer.play("broadcast_fade_in")
 	
-func show_hint(hint_message):
+func show_hint(_hint_message):
 	pass
 	
 func _on_broadcast_timer_timeout():
 	%AnimationPlayer.play("broadcast_fade_out")
 
 func save_game():
-	pass
+	var save_dict = {
+		"level_data": {
+			0: { # tutorial
+				"completed": false,
+				"last_checkpoint": Vector2(0, 0)
+			},
+			1: {
+				"completed": false,
+				"last_checkpoint": Vector2(0, 0)
+			},
+			2: {
+				"completed": false,
+				"last_checkpoint": Vector2(0, 0)
+			},
+		},
+		"deaths": 0
+	}
 
